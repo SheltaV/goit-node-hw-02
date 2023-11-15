8992e68236732e4169db5f74d68824a4f5734ebe
SheltaV <bluegiraffezaoz@gmail.com>
2023-11-15 16:43:13 +0200

    move db_host to process.env

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-import { app } from './app.js'
+import mongoose from 'mongoose';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+import { app } from './app.js';
+
+const HOST_DB = process.env;
+
+mongoose.connect(HOST_DB)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful")
+    })
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+  
